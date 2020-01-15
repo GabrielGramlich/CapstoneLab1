a07dcb3fec69f3a6429d25f90aee58d01467a66e
GabrielGramlich <mb4510qj@go.minneapolis.edu>
2020-01-15 14:53:32 -0600

    Updated a file that was duplicated and missing its actual code.

---
+++ classes.py
@@ -0,0 +1,11 @@
+count = int(input('How many classes are you taking this semester? '))
+counter = 0
+classes = []
+
+while (counter < count):
+    classes.append(input('Enter the name of class #' + str((counter + 1)) + ': '))
+    counter += 1
+
+print('The classes you are taking:')
+for item in classes:
+    print(item)
