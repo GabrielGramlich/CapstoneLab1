1ae58367e38afa362c3609b74c45c6a6324d486d
GabrielGramlich <mb4510qj@go.minneapolis.edu>
2020-01-15 15:46:35 -0600

    There was just a syntax error the error message wasn't leading me on to. Fixed.

--- camelTwo.py
+++ camelTwo.py
@@ -1,13 +1,15 @@
+# This was to attempt to convert camel.py into a list comprehension, but nope. I'll work on this later.
+
 words = input('Please input a sentence: ').split()
-first_word = str(words[0].lower)
+first_word = words[0].lower()
+camel_case = first_word
+
 rest_of_words = words[1:]
 rest_of_words_capitalized = [word[0:1].upper() + word[1:len(word)].lower() for word in rest_of_words]
-camel_case = first_word
 
 for word in rest_of_words_capitalized:
     camel_case += word
 
-# Removing symbols we don't want
 camel_case = camel_case.replace('.', '')
 camel_case = camel_case.replace('#', '')
 camel_case = camel_case.replace('/', '')
